3cf07cd4ceef164446628750dcd35253de10b736
Michael Fitzpatrick <mikefitz888@gmail.com>
2016-11-10 17:37:32 +0000

    manager bug fix, GameObject base class defined

---
+++ include/gameobject.h
@@ -0,0 +1,20 @@
+#ifndef GAMEOBJECT_H
+#define GAMEOBJECT_H
+
+#include "RenderManager.h"
+
+namespace gameobject {
+	using size_t id_t;
+
+	class GameObject : public virtual IRenderable {
+		const id_t id_;
+	public:
+		GameObject(id_t id); //Very important to get key from manager (for memory management + networking)
+		id_t getID();
+	};
+
+	GameObject::GameObject(id_t id) : id_(id) {}
+	id_t GameObject::getID() { return id_; }
+}
+
+#endif //GAMEOBJECT_H

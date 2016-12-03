833cafe284ef3b006c3b090268d9026f7d48512a
Michael Fitzpatrick <mikefitz888@gmail.com>
2016-12-03 15:42:39 +0000

    forgot to add files

---
+++ src/towers/BasicTower.cpp
@@ -0,0 +1,28 @@
+#include "../../include/tower.h"
+
+namespace tower {
+	class BasicTower : Tower {
+		sf::Texture* texture;
+	public:
+		BasicTower(id_t key, Manager* m);
+		void init();
+		void render();
+	};
+
+	BasicTower::BasicTower(id_t key, Manager* m) : Tower(key, m) {
+		x = 200;
+		y = 400;
+	}
+
+	void BasicTower::init(){
+		render_manager = manager->getRenderManager();
+		texture = new sf::Texture();
+		if(!texture->loadFromFile("Resources/Textures/Towers/Basic.jpg")){
+			std::cout << "[ERROR] Could not load texture! (BasicTower)" << std::endl;
+		}
+	}
+
+	void BasicTower::render(){
+		
+	}
+}
---
+++ src/unit.cpp
@@ -0,0 +1,8 @@
+#include "../include/unit.h"
+
+namespace unit {
+	void Unit::init() {}
+	void Unit::render() {}
+	void Unit::renderGUI() {}
+	void Unit::release() {}
+}

0b2bb73ef6c4e2e43e1c030dd2aab1b9854a3c7a
Michael <mp14959@bristol.ac.uk>
2017-01-25 16:46:32 +0000

    Changed healthbar texture and shader

--- src/Resources/Shaders/health_bar_mask_shd_frag.glsl
+++ src/Resources/Shaders/health_bar_mask_shd_frag.glsl
@@ -15,9 +15,11 @@ void main(void) {
     vec4 base = texture(texture_diffuse, pass_TextureCoord);
 	vec4 mask = texture(texture_mask, pass_TextureCoord);
 
+	float alpha_fac = 1.0;
 	if( mask.r > healthProgress ){
-		discard;
+		alpha_fac = clamp(1.0f - (mask.r-healthProgress)/0.1f, 0.0f, 1.0f);
 	}
 
     out_Color = pass_Color*drawColour*base;
+	out_Color.a = alpha_fac*base.a;
 }
